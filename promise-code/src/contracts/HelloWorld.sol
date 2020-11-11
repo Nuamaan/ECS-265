351adebf09e24f444b1b5d3e6b6a7d72e5a9c5a2
Nuamaan <62098979+Nuamaan@users.noreply.github.com>
2020-11-11 20:18:18 +0530

    Update HelloWorld.sol

--- promise-code/src/contracts/HelloWorld.sol
+++ promise-code/src/contracts/HelloWorld.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.5.1;
 contract Promise{
     uint256 public totPromise;//to index the promises
     address payable manager;
-    mapping(uint256=>Prom) unConfirmedProm;// to hold unconfirmed promises.
+    mapping(uint256=>Prom) unConfirmedProm;// to hold unconfirmed promises initially before they are signed by the participating parties.
     mapping(uint256=>Prom) ConfirmedProm;// to hold confirmed promises.
+    mapping(uint256=>Prom) rejectedProm;// to hold rejected promises i.e promises rejected by any one party.
+    
     
     constructor() public {
         manager=msg.sender;
     }
+    
+    
     
     
     modifier isManager(){
@@ -16,18 +20,28 @@ contract Promise{
         _;
     }
     
-    /* this event is created to notify the other party of the promise created*/
+    
+    
+    /* this event is to notify the other participating party of the promise being created*/
     event notify(
         address P2,
-        uint256 ind
+        uint256 ind,
+        string oath
         );
     
     
+    
+    
+    /* a struct that holds the address and the boolean variable annotating that parties commitment status*/
     struct partyStatus{  
         address P;
         bool commitment;
     }
     
+    
+    
+    
+    /*a structure holding our promise and the parties involved and their commitment towards the promise*/
     struct Prom{
         uint256 promIndex;
         partyStatus P1;
@@ -37,30 +51,44 @@ contract Promise{
         
     }
     
-    /*this function is used by one of the parties to create the unConfirmedPrompromise,
-    the address of the second party and the oath are also passed as parameteres.*/
+   
+   
+   
+   
+    /*this function is used by one of the parties called a builder to create the unConfirmedPrompromise,
+    the address of the second party and the oath are also passed as parameters.*/
+    /* no malicious builder would want to call this function because this function takes in ehters and passes them to the manager.*/
      
     function addPromise(address builder,address P2,string memory _oath) public payable {
-        require(msg.sender==builder || msg.sender==P2);
+        require(msg.sender==builder);
         totPromise++;
 
         unConfirmedProm[totPromise]=Prom(totPromise,partyStatus(builder,true),partyStatus(P2,false), _oath,false);
-        manager.transfer(msg.value);
-        emit notify(P2,totPromise); 
+        
+        manager.transfer(msg.value);        /*ethers are passed to the manager,,,calling addPromise function would cost the builder*/
+        
+        emit notify(P2,totPromise,_oath);   /* the event is emitted,,,, the other part gets to know about the promoise being created 
+                                            by its name as one of the involved parties*/
         
         
     }
     
-    /*this function is used by the other party to view the promise created by the first party*/
+    
+    
+   
+   
+    /*this function is used by the involved parties to view the promise */
     
     function viewPromise(uint256 ind) public view returns(string memory){
-        require(msg.sender==unConfirmedProm[ind].P2.P || msg.sender==unConfirmedProm[ind].P1.P || msg.sender==manager);
+        require(msg.sender==unConfirmedProm[ind].P2.P || msg.sender==unConfirmedProm[ind].P1.P || msg.sender==manager||msg.sender==ConfirmedProm[ind].P2.P || msg.sender==ConfirmedProm[ind].P1.P);
         return unConfirmedProm[ind].oath;
         
         
         
         
     }
+    
+    
     
     
     
@@ -74,8 +102,27 @@ contract Promise{
         manager.transfer(msg.value);
     }
     
-   
-   /* this function is used to confirm promises and transfer them from
+    
+    
+    
+    
+    
+    /*this function is used by the non-builder party to reject the promise. 
+    The promise after being rejected is added to the rejectPromise mapping */
+    function rejectPromise(uint256 ind) public payable {
+        require(unConfirmedProm[ind].P2.P==msg.sender);
+        unConfirmedProm[ind].status=false;
+        rejectedProm[ind]=unConfirmedProm[ind];
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+/* this function is used to confirm promises and transfer them from
    unConfirmedProm to ConfirmedProm mapping*/
    
     function confirmpromise(uint256 ind) internal {
