08837070fb1359e845790a1ae686e691b1eec4ab
ikechicodes <37428088+ikechicodes@users.noreply.github.com>
2020-11-16 11:45:12 -0800

    Metamask Test Page

---
+++ promise-frontend/src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+ <metamask/>
+</template><script>
+
+import Metamask from '../components/Metamask'
+export default {
+ name: 'test',
+ beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+  },
+ components: {
+ 'metamask': Metamask
+ }
+}
+</script>
+
+<style scoped>
+</style>
